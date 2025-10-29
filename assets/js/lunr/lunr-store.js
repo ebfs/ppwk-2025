2418d2c4053390d18c4c899d4a64bd37f014b31b
ebfs <ebfs@users.noreply.github.com>
2025-10-29 20:41:18 +0000

    deploy: 1bee9692a04b8d23036de78decdcbf460b587e1a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,36 +2,36 @@ var store = [{
         "title": "Viikko 1",
         "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
         "tags": [],
-        "url": "/vko1/",
+        "url": "/ppwk-2025/vko1/",
         "teaser": null
       },{
         "title": "Viikko 2",
         "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
         "tags": [],
-        "url": "/vko2/",
+        "url": "/ppwk-2025/vko2/",
         "teaser": null
       },{
         "title": "Viikko 3",
         "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
         "tags": [],
-        "url": "/vko3/",
+        "url": "/ppwk-2025/vko3/",
         "teaser": null
       },{
         "title": "Viikko 4",
         "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
         "tags": [],
-        "url": "/vko4/",
+        "url": "/ppwk-2025/vko4/",
         "teaser": null
       },{
         "title": "Viikko 5",
         "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
         "tags": [],
-        "url": "/vko5/",
+        "url": "/ppwk-2025/vko5/",
         "teaser": null
       },{
         "title": "Viikko 6",
         "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
         "tags": [],
-        "url": "/vko6/",
+        "url": "/ppwk-2025/vko6/",
         "teaser": null
       },]
