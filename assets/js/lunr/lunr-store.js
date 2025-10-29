a824e4d04467b714a04cb9fca20b1adc398ecf54
ebfs <ebfs@users.noreply.github.com>
2025-10-29 20:28:59 +0000

    deploy: e7b7c10efa58b1fc6c81493e46d8cfe0bde3fc51

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,37 @@
+var store = [{
+        "title": "Viikko 1",
+        "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
+        "tags": [],
+        "url": "/vko1/",
+        "teaser": null
+      },{
+        "title": "Viikko 2",
+        "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
+        "tags": [],
+        "url": "/vko2/",
+        "teaser": null
+      },{
+        "title": "Viikko 3",
+        "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
+        "tags": [],
+        "url": "/vko3/",
+        "teaser": null
+      },{
+        "title": "Viikko 4",
+        "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
+        "tags": [],
+        "url": "/vko4/",
+        "teaser": null
+      },{
+        "title": "Viikko 5",
+        "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
+        "tags": [],
+        "url": "/vko5/",
+        "teaser": null
+      },{
+        "title": "Viikko 6",
+        "excerpt":"ABDEFG   This is my first Jekyll site!  ","categories": [],
+        "tags": [],
+        "url": "/vko6/",
+        "teaser": null
+      },]
