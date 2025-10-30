d5a562809d8b974b5ead31a28924a0ff6be62113
ebfs <ebfs@users.noreply.github.com>
2025-10-30 11:10:18 +0000

    deploy: 046d0437380b5385bb619e931676922c739b73d7

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Harjoitustyö",
-        "excerpt":" ","categories": [],
-        "tags": [],
-        "url": "/ppwk-2025/harjoitustyo/",
-        "teaser": null
-      },{
         "title": "Viikko 1",
         "excerpt":"Lämmittely   Mitäs tänne kannattaisi edes laittaa? Vanha sivusto joka käytti index.html ja styles.css ei ole enää käytössä.   GitHubissa vanhat tiedostot löytyvät täältä  ","categories": [],
         "tags": [],
