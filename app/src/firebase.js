81f27ad0100fae506b73d15c0e2f5ef63de79e37
ebfs <ebfs@users.noreply.github.com>
2025-11-02 12:06:54 +0000

    deploy: 1b99de3d1f20d24a57db804d96b6a4d83dc1e2a9

--- app/src/firebase.js
+++ app/src/firebase.js
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD0f97tGu0i4rGooGpTlH3UQaQKuHRyCdA",
-  authDomain: "ppwk-community-event-hub.firebaseapp.com",
-  projectId: "ppwk-community-event-hub",
-  storageBucket: "ppwk-community-event-hub.firebasestorage.app",
-  messagingSenderId: "880036105881",
-  appId: "1:880036105881:web:4df23c20c80b438c5c7208",
-  measurementId: "G-1RE2RKXQYE"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
